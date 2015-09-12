2159e4d5688e8892f343506f646cf9c3135b84fb
Stephen Matta <stephenjmatta+github@gmail.com>
2015-09-11 22:50:02 -0400

    Added sanjo:jasmine. Temporarily turning off integration tests until we have some.

--- continuous_integration.sh
+++ continuous_integration.sh
@@ -17,17 +17,17 @@ export CUCUMBER_TAIL=1
 
 echo "Running against PhantomJS..."
 export CUCUMBER_TAGS="~@chrome"
-meteor --test || touch FAILURE
+# meteor --test || touch FAILURE
 
 echo "Running against Chrome..."
 export CHIMP_OPTIONS="--browser=chrome"
 export CUCUMBER_TAGS=""
-meteor --test || touch FAILURE
+# meteor --test || touch FAILURE
 
 echo "Running against Firefox..."
 export CHIMP_OPTIONS="--browser=firefox"
 export CUCUMBER_TAGS=""
-meteor --test || touch FAILURE
+# meteor --test || touch FAILURE
 
 echo "Finished running integration tests..."
 if [[ -e FAILURE ]];then
